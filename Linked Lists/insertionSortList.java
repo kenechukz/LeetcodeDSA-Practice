7857b80703c253b414b1b68d154abe0d7d25c73e
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-04-14 16:38:14 +0100

    Update insertionSortList.java

--- Linked Lists/insertionSortList.java
+++ Linked Lists/insertionSortList.java
@@ -1,45 +1,39 @@
+/*
+* Check if nodes after dummy (prev.next) exists and nodes after dummy less than current node
+* If true we change prev node till we find a node bigger than currentstore a pointer to current node's next node
+* We assign current node's next node pointer to prev.next (so it's behind prev.next as it is smaller than it)
+* We then add current node to end of prev node pointer
+* Finally we let cur = nextNode
+* Return head, which is node after dummy node
+*/
+    
+
 
 public class insertionSortList {
     public static ListNode insertionSort(ListNode head) {
         if (head == null || head.next == null) return head;
-
         ListNode dummy = new ListNode(0);
-
         ListNode cur = head;
-
         while (cur != null){
-
             ListNode prev = dummy;
-
-            // Check if nodes after dummy (prev.next) exists and nodes after dummy less than current node
-            // if true we change prev node till we find a node bigger than current
-
             while (prev.next != null && prev.next.val < cur.val){
                 prev = prev.next;
             }
-
-            // store a pointer to current node's next node
-            // we assign current node's next node pointer to prev.next (so it's behind prev.next as it is smaller than it)
-            // we then add current node to end of prev node pointer
-            // finally we let cur = nextNode
-
             ListNode nextNode = cur.next;
             cur.next = prev.next;
             prev.next = cur;
-
             cur = nextNode;
-
         }
-        // return head, which is node after dummy node
         return dummy.next;
     }
 
+
+    // Implementation: e.g. 4 -> 2 -> 1 -> 3
     public static void main(String[] args){
         ListNode node2 = new ListNode(3);
         ListNode node3 = new ListNode(1, node2);
         ListNode node = new ListNode(2, node3);
         ListNode head = new ListNode(4, node);
-
         System.out.println("Before: ");
         ListNode cur = head;
         while (cur != null) {
