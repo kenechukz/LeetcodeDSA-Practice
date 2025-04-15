7561026f4d97fef6e04a10747f31ded8b321ae83
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-04-15 20:18:53 +0100

    Difficulty: Medium Language:  .java  Time: 3 ms (90.78%), Space: 46.8 MB (32.28%)

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int L = 0, zeroCnt = 0, maxConsc = 0;
+
+        for (int R = 0; R < nums.length; R++) {
+            if (nums[R] == 0) {
+                zeroCnt++;
+            }
+
+            while (zeroCnt > k) { // Shrink window when zero count exceeds k
+                if (nums[L] == 0) {
+                    zeroCnt--;
+                }
+                L++;
+            }
+
+            // Update maxConsc for every valid subarray
+            maxConsc = Math.max(maxConsc, R - L + 1);
+        }
+
+        return maxConsc;
+    }
+}
