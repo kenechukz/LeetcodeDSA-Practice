d152bbaac41c07424a1d6216ee17c96d436b6fbb
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-04-15 20:23:33 +0100

    Difficulty: Medium Language:  .java  Time: 3 ms (79.17%), Space: 56.2 MB (90.83%)

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+
+        int maxSub = 0;
+        int zeroCnt = 0;
+        int L = 0;
+
+
+        for (int R = 0; R < nums.length; R++){
+
+            
+
+            if (nums[R] == 0){
+                zeroCnt++;
+            }
+            
+        
+            while (zeroCnt > 1){
+                zeroCnt -= (nums[L] == 0 ? 1 : 0);
+                L++;
+            }
+
+            // Find all subarrays
+            maxSub = Math.max(maxSub, R-L);
+
+        }
+
+        // issue with oneCnt reset
+
+        return maxSub;
+
+
+
+
+        
+
+
+        
+
+
+        
+    }
+}
