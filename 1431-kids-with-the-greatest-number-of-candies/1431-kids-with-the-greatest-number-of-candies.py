99c3ea4419b8b39ced927a30dff317d61ef93338
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-04-15 20:30:33 +0100

    Difficulty: Easy Language:  .py  Time: 0 ms (100%), Space: 17.8 MB (34.81%)

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+        maxCandies = max(candies)
+        
+        
+        
+
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= maxCandies:
+                candies[i] = True
+
+            else:
+                candies[i] = False
+
+
+        return candies
