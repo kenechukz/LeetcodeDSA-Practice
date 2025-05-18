71b7978234f4f0eae4d2404003583b921a54730c
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-05-18 22:39:19 +0100

    Difficulty: Medium Language:  .py  Time: 0 ms (100%), Space: 17.7 MB (84.89%)

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -2,9 +2,23 @@ class Solution:
     def decodeString(self, s: str) -> str:
 
 
-        # "3[a12[bc]]"
+        
 
-        # "3"
+        # We iterate through s, adding chars in s to stack as long as the char is not ']'
+        # Once we hit ']', we move down the stack and pop the letters, adding them to a substring 
+        # till we hit '[' and pop it
+        # We then move down stack to find k (integer) and pop digits, adding them to k digit by digit
+        # When we hit a non digit we now append the result of "substring * int(k)" to stack
+        # We continue till we reach end of s
+
+        # Example:
+        # for "3[a12[bc]]" we do subproblem first: 12[bc] -> bcbcbc...bc
+        # stack after first subproblem: 3[abcbcbc...bc]
+        # stack after second subproblem (final): abcbcbc...bcabcbcbc...bcabcbcbc...bc
+        # Time Complexity: O(n)
+        # Space Complexity: O(n) ~ stack for auxillary memory
+
+        
 
         st = []
 
