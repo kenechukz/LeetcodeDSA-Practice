dba0108f62c355054c6a463e2b018f29564c28ef
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-05-18 16:27:43 +0100

    Difficulty: Medium Language:  .py  Time: 172 ms (16.08%), Space: 18.9 MB (77.08%)

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -6,22 +6,24 @@ class Solution:
         
         #given string which can contains *
 
-        # if char is star remove top of stack and remove star
+         
+        # if current char in string is a star '*' and stack exists 
+        # we pop top of stack
+        # otherwise we add that char the stack 
+        # at the end we join the array into a string
+        # Time complexity: O(n) ~ Iterating through characters in string takes n iterations 
+        # n being the length of the string
+        # Space complexity: O(n) ~ Auxillary memory in use of stack which is of size n
 
         stack = []
-
-
         for i in range(len(s)):
-
 
             if s[i] == "*":
                 if stack:
                     stack.pop()
-
             else:
                 stack.append(s[i])
 
-        
         return ''.join(stack)
         
                 
