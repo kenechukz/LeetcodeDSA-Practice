f693e182e6acfb0bf9ddc5c04d544d08e367a2a0
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-05-19 23:01:05 +0100

    Difficulty: Easy Language:  .py  Time: 0 ms (100%), Space: 17.7 MB (94.16%)

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+
+
+        # a node is a leaf if it has no children
+
+
+        leaves1 = []
+        leaves2 = []
+
+        def findLeaves(root, leaves):
+
+            if root == None:
+                return leaves
+
+            if root.left == None and root.right == None:
+                leaves.append(root.val)
+                return leaves
+
+
+            findLeaves(root.left, leaves)
+            findLeaves(root.right, leaves)
+
+            return leaves
+
+
+        return findLeaves(root1, leaves1) == findLeaves(root2, leaves2)
+        
+
+                 
+
+            
+
+
+        
