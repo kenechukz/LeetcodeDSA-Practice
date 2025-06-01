4487eae2e2b689c1a3693a19daa35acb23cebb3d
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-06-01 15:24:04 +0100

    Difficulty: Medium Language:  .py  Time: 494 ms (85.24%), Space: 29.7 MB (96.34%)

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -4,21 +4,15 @@ class Solution:
         l = 0
         r = len(nums)-1
         count = 0
-
-
         while l < r:
-
             if nums[l] + nums[r] == k:
                 l+= 1
                 r-= 1
                 count+= 1
-
             elif nums[l] + nums[r] > k:
                 r-= 1
-
             elif nums[l] + nums[r] < k:
                 l+= 1
-
         return count
 
             
