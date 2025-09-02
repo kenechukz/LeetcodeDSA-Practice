74a927c67dfbf98359be5d56aadb1c9e57ddd462
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-09-02 15:03:22 +0100

    Time: 0 ms (100%), Space: 17.8 MB (45.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,32 +2,14 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
         """
-        R:
-        Given m rows and n columns
-        return no. of ways to reach grid[m-1][n-1] 
+        We start from the finish point and work backwards to determine the "recursive rule"
+        We now all the rows and columns aligned with the finish point (as in row m-1 and col n-1) 
+        will have 1 way of reaching finish. 
+        Because of this we start from row m-2 and calculate it's no. of ways of reaching finish by
+        summing the cell directly below cur cell with column 1 ahead of cur cell.
 
-        E:
-
-        |R|
-        if grid[0][0] == grid[m-1][n-1]
-
-        if row == m-1:
-            only move right
-
-
-
-        |R | |
-        m = 1, n = 2
-        need to validate move down and move right are on grid
-
-        at each step we have a choice, move down or move right
-
-
-        Recursive Rule:
-        we start with 1 x 1, then expand by 1 each time
-        1 x 1 = 0
-        2 x 2 = 2
-        3 x 3 = 
+        Time: O(n * m) 
+        Space: O(n)
         """
 
         prevRow = [1] * n
