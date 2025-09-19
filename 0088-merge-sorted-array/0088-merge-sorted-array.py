cea79afd632582a58abc017de7ffccf5f5c02c1b
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-09-19 20:16:49 +0100

    Time: 0 ms (100%), Space: 17.9 MB (43.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
@@ -13,20 +14,57 @@ class Solution:
         if n == zero -> do nothing
 
         A:
-
+        #1
         nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6]
                        3 
         add num2 to nums1 from index m to end of list - O(m)
         sort - (n log n)
+
+        #2
+        nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6]
+                     l                         
+                                               r
+        sorted_array [1, 2, 2]
+
+        ...
+        nums1 = sorted_array
+
+
         """
 
+        sorted_array = []
         if n == 0:
             return None
 
-        for i in range(m, m+n):
-            nums1[i] = nums2[i-m]
+        n1,n2 = 0,0
+        while n1 < m and n2 < n:
+            if nums1[n1] <= nums2[n2]:
+                sorted_array.append(nums1[n1])
+                n1+=1
+            else:
+                sorted_array.append(nums2[n2])
+                n2+=1
 
-        nums1.sort()
+        if n1 >= m and n2 < n:
+            while n2 < n:
+                sorted_array.append(nums2[n2]) 
+                n2+=1
+
+        if n2 >= n and n1 < m:
+            while n1 < m:
+                sorted_array.append(nums1[n1]) 
+                n1+=1
+        
+        print(sorted_array)
+        for i in range(m+n):
+            nums1[i] = sorted_array[i]
+
+        
+
+
+
+
+        
 
 
 
