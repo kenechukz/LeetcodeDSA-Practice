20004bbfd1099769d313c7073c4dadc86e487b86
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-09-20 17:05:54 +0100

    Time: 15 ms (10.1%), Space: 24.4 MB (7.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,46 +1,23 @@
-import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        """
-        answer is unique
-        R:
-        
-        return top k freq elems
-
-        constraints: 1 <= k <= len(nums)
-        E:
-        if len(nums) == 1
-        [1, 1, 2, 2, 3 ,3]
-
-        set
-        tuple
-        not in set
-            set: {1, 2}
-        lst: [(2, 1), (2, 2), (2, 3)] - O(n)
-        maxHeap [(-2, 1), (-2, 2), (-2, 3)] - O(n)
-        pop k times from heap - k * log(n)
-        k = 2
-        """
-
-        seen = dict()
-        lst = []
         n = len(nums)
-        if n == 1 and k == 1:
-            return nums
-        j = 0
-        for i in range(n):
-            x = nums[i]
-            if not x in seen:
-                seen[x] = j
-                lst.append([-1, x])
-                j+=1
-            else:
-                lst[seen[x]][0] -=1
+        # Step 1: count frequencies
+        freq_map = Counter(nums)   # O(n)
 
-        heapq.heapify(lst)
-        res = [(heapq.heappop(lst))[1] for _ in range(k)] 
-        return res
+        # Step 2: bucket where index = frequency
+        bucket_arr = [[] for _ in range(n+1)]
 
-
+        # O(n)
+        for num, freq in freq_map.items():
+            bucket_arr[freq].append(num)
         
+        # pop k freq elems
+        res = []
+
+        for i in range(n, -1,-1):
+            bucket = bucket_arr[i]
+            for num in bucket:
+                res.append(num)
+                if len(res) == k:
+                    return res
+            
