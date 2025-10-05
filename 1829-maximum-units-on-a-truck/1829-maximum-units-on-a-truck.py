440cd73affe8d13e6b9a1eafbd5a1a9a101e4ac3
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-10-05 19:27:34 +0100

    Time: 12 ms (22.69%), Space: 18.2 MB (33.08%) - LeetHub

---
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
@@ -0,0 +1,59 @@
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+
+        """
+        R:
+
+
+
+        return max no. units that can be put in truck (as long as no. boxes < truckSize)
+
+        E:
+
+        if boxTypes.length == 1:
+
+        
+        A:
+
+        boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+
+        unitsPerBox {0: 3, 1: 2, 2: 1}
+
+        1 * 3 + 2* 2 + 1 * 1 = 8
+
+
+        """
+
+        boxTypes.sort(key= lambda x: x[1], reverse=True)
+
+        res = 0
+
+        print(boxTypes)
+        for i in range(len(boxTypes)):
+            boxes = boxTypes[i][0]
+            units = boxTypes[i][1]
+            boxesUsed = boxes
+
+
+            if truckSize == 0:
+                return res
+
+            if boxes > truckSize:
+                boxesUsed = truckSize
+
+            res += boxesUsed * units
+            truckSize -= boxesUsed
+
+        return res
+
+        
+
+            
+
+            
+
+
+
+
+
+
