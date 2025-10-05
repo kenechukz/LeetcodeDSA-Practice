1f6c03f75a8cfddb3c974a5c16dd0edd09a530e9
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-10-05 20:51:48 +0100

    Time: 422 ms (27.11%), Space: 36.4 MB (35.62%) - LeetHub

---
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,53 @@
+from collections import Counter
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+
+        """
+        R:
+
+        return least number of unique elems left after k removals
+
+        E:
+        0 <= k <= arr.length
+
+        A:
+
+        arr = [4,3,1,1,3,3,2], k = 3
+
+         4: 1, 2: 1, 1: 2, 3: 3
+        total unique = len(dict)
+
+        {(4, 1), (2, 1), (1, 2),(3, 3)}
+
+        
+        """
+
+
+        count = Counter(arr)
+        unique = len(count)
+
+        arr = [[k, v] for k,v in count.items()]
+        arr.sort(key=lambda x: x[1])
+
+        i,j = 0,0
+        
+        while i < k and j < len(arr):
+            arr[j][1] -= 1
+            if arr[j][1] == 0:
+                unique -=1
+                j+=1
+            print(i,j)
+            i+=1
+
+        return unique
+
+
+
+
+
+
+
+
+
+
+
