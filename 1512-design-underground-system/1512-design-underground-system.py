0cc474db9a016a7ccb93beefbf82b637b3f652e7
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-10-26 21:00:58 +0000

    Time: 18 ms (81.17%), Space: 28.7 MB (96.44%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.py
+++ 1512-design-underground-system/1512-design-underground-system.py
@@ -1,75 +1,22 @@
 class UndergroundSystem:
-    """
-    R:
-    average travel times between different stations
-
-    getAverageTime(string startStation, string endStation) - Returns the average time it takes 
-    to travel from startStation to endStation.
-
-
-    when customer checks out -> stations[endStation] = {startStation: [end_time - start_time]}
-
-
-    call to getAverageTime -> sum(stations[endStation][startStation]) / no. trips (len of stations[endStation]
-    [startStation])
-
-    """
-
     def __init__(self):
-        
-        # customers[id] = [start_time, checked_in?, prev_station]
-        self.customers = {}
-        # when customer checks out -> stations[endStation] = {startStation: [end_time - start_time]}
-        self.stations = {}
-        
-
+        self.customers = {}  # id -> [start_time, start_station]
+        self.stations = {}   # end_station -> {start_station: [trip_times]}
+    
     def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.customers[id] = [t, stationName]
+    
+    def checkOut(self, id: int, end_station: str, t: int) -> None:
+        start_time, start_station = self.customers[id]
+        del self.customers[id]  # clean up after checkout
 
-        if id not in self.customers:
-            self.customers[id] =[-1, False, stationName]
-
-        if stationName not in self.stations:
-            self.stations[stationName] = {}
-
-        # check if customer is checked in already
-        elif self.customers[id][1] == True:
-            return
-
-        self.customers[id][0] = t 
-        self.customers[id][1] = True  
-        self.customers[id][2] = stationName  
-
-    def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start_station = self.customers[id][2]
-        start_time = self.customers[id][0]
-
-        if stationName not in self.stations:
-            self.stations[stationName] = {}
-
-        if start_station not in self.stations[stationName]:
-            self.stations[stationName][start_station] = []
-
-        self.stations[stationName][start_station].append(t- start_time)
-        self.customers[id][1] = False
-
+        if end_station not in self.stations:
+            self.stations[end_station] = {}
+        if start_station not in self.stations[end_station]:
+            self.stations[end_station][start_station] = []
         
-
+        self.stations[end_station][start_station].append(t - start_time)
+    
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        
-        total_time = sum(self.stations[endStation][startStation])
-        trips = len(self.stations[endStation][startStation])
-
-        return total_time / trips
-
-
-        
-        
-
-
-# Your UndergroundSystem object will be instantiated and called as such:
-# obj = UndergroundSystem()
-# obj.checkIn(id,stationName,t)
-# obj.checkOut(id,stationName,t)
-# param_3 = obj.getAverageTime(startStation,endStation)
-
-
+        times = self.stations[endStation][startStation]
+        return sum(times) / len(times)
