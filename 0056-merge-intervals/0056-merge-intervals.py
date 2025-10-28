8f050ac22b2cfd60c1f13dbdb104388b3ae80c28
Declan Kene Chukwu <148201350+kenechukz@users.noreply.github.com>
2025-10-28 20:39:45 +0000

    Time: 10 ms (34.73%), Space: 21 MB (64.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,42 +1,49 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
         """
         R:
-        intervals - non sorted
 
-        return array of non-overlap intervals
-        constraints: 1 <= len(intrvals) <= 10^4
+
+        return array of non-overlapping intervals
 
         E:
+        is input sorted ? NO
         if len(intervals) == 1:
             return intervals
 
         A:
-        [[1,3],[2,9],[8,10],[15,18]]
-        iter: i <- 1 to n
-        prev [1,3]
-        cur [2,9]
-        [[1,3],[2,9],[8,12],[11,18]]
-        if cur[0] < prev[1]
-            merge
-            cur = [min(start), max(end)]
-        [[1,18]]
+        non overlapping if cur.start >= prev.end:
+        [1,3] [3, 5]
 
+
+        res = [[1, 3], [3, 5]]
+
+        [[1,3],[2,6],[8,20],[9,18]]    
+
+        res = [[1,6], [8, 20]]
         """
-        n = len(intervals)
+
+            
         res = []
-        if n == 1:
-            return intervals
         intervals.sort()
-        prev = intervals[0]
-        for i in range(1,n):
-            cur = intervals[i]
-            if cur[0] <= prev[1]:
-                cur = [prev[0], max(prev[1],cur[1])]
+
+        for it in intervals:
+            start = it[0]
+            end = it[1]
+            if not res:
+                res.append(it)
+            elif start <= res[-1][1]:
+                res[-1] = [res[-1][0], max(res[-1][1], end)]
+                
             else:
-                res.append(prev)
-            prev = cur
-        res.append(prev)
+                res.append(it)
+
+        return res
+
+
+
+
 
         return res
         
